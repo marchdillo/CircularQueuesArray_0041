88286c5d75ab830a65c8aa1f6d4eafaa5f72be28
marchdillo <kevinno.limiy@gmail.com>
2025-06-12 11:14:21 +0700

    menambahkan void remove else

--- Queues.cpp
+++ Queues.cpp
@@ -54,5 +54,12 @@ public:
             FRONT = -1;
             REAR = -1;
         }
+        else
+        {
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
     }
 };
