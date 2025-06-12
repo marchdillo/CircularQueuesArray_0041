c6d8690f6cb63b808137ba395e415799abdb0a19
marchdillo <kevinno.limiy@gmail.com>
2025-06-12 11:19:13 +0700

    menambahkan if while front position dan cout

--- Queues.cpp
+++ Queues.cpp
@@ -75,5 +75,15 @@ public:
         }
 
         cout << "\nElements in the queue are...\n";
+
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 };
